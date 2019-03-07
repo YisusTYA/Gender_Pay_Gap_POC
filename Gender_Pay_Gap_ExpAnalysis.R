50f19a6b4be87d72ebf0cca977f18bb3d961dc3e
Daniel <aferridanile@gmail.com>
2019-03-07 17:57:00 +0100

    Adding plots
    I have added plots of Gross Salary, Net Salary y Extraordinary Payments.

--- Gender_Pay_Gap_ExpAnalysis.R
+++ Gender_Pay_Gap_ExpAnalysis.R
@@ -1,5 +1,11 @@
-#Load Library
-library(tidyverse)
+#Loading Libraries
+ #library(tidyverse)
+library(ggplot2)
+library(purrr)
+library(tibble)
+library(dplyr)
+library(readr)
+
 #Loading data
 data_female<- read_delim("Brut_Mon_sal_composition_Female.csv", 
                          "\t", escape_double = FALSE, col_names = FALSE, 
@@ -7,7 +13,7 @@ data_female<- read_delim("Brut_Mon_sal_composition_Female.csv",
                          )
 view(data_female)
                                                   
-data_male<- read_delim("Brut_Mon_sal_composition_Male.csv.csv", 
+data_male<- read_delim("Brut_Mon_sal_composition_Male.csv", 
                          "\t", escape_double = FALSE, col_names = FALSE, 
                          col_types = cols(X12 = col_skip()), trim_ws = TRUE
 )
@@ -45,17 +51,46 @@ Gathered_tidy_data<- tidy_data %>%
          key = 'Wage Type',value = 'Quantity')
 
 View(Gathered_tidy_data)
-write.csv(x = Gathered_tidy_data,'Gathered_tidy_data')
+write.csv(x = Gathered_tidy_data,'Gathered_tidy_data.csv',fileEncoding = 'utf8')
+
 
 #START PLOTTING
 
 
-#1: GROUP DATA
+#Basic Salary
 
-to_plot<-Gathered_tidy_data %>%
+Grouped_Basic_salary<-Gathered_tidy_data %>%
   filter(`Wage Type`=='Basic Salary') %>%
   group_by(Gender)
   
-ggplot(data = to_plot)+
-  geom_bar(mapping = aes(x=`Autonomous Region`,y=Quantity,fill=Gender),stat = 'identity',position = 'dodge') + coord_flip()
+ggplot(data = Grouped_Basic_salary)+
+  geom_bar(mapping = aes(x=`Autonomous Region`,y=Quantity,fill=Gender),stat = 'identity',position = 'dodge') + coord_flip() + ggtitle('Basic salary by gender and Autonomous Region')
+
+#Net Salary
+
+Grouped_Net_Salary <- Gathered_tidy_data %>%
+  filter(`Wage Type`=='Net salary') %>%
+  group_by(Gender)
+
+ggplot(data = Grouped_Net_Salary)+
+  geom_bar(mapping = aes(x=`Autonomous Region`,y=Quantity,fill=Gender),stat = 'identity',position = 'dodge') + coord_flip() + ggtitle('Net salary by gender and Autonomous Region')
+
+#Extraordinary payments
+
+
+Grouped_Extraor_payments <- Gathered_tidy_data %>%
+  filter(`Wage Type`=='Extraordinary payments') %>%
+  group_by(Gender)
+
+ggplot(data = Grouped_Extraor_payments)+
+  geom_bar(mapping = aes(x=`Autonomous Region`,y=Quantity,fill=Gender),stat = 'identity',position = 'dodge') + coord_flip() + ggtitle('Extraordinary payments by gender and Autonomous Region')
+
+#Gross salary
+
+Grouped_Gross_salary <- Gathered_tidy_data %>%
+  filter(`Wage Type`=='Gross Salary') %>%
+  group_by(Gender)
+
+ggplot(data = Grouped_Extraor_payments)+
+  geom_bar(mapping = aes(x=`Autonomous Region`,y=Quantity,fill=Gender),stat = 'identity',position = 'dodge') + coord_flip() + ggtitle('Gross salary payments by gender and Autonomous Region')
 
